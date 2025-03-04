ae736c6e14d076856cf8d65d4fd88f1471b6a230
nekodamasi34 <117162125+nekodamasi34@users.noreply.github.com>
2025-03-04 13:44:39 +0900

    Mecanum.hpp update

--- Mecanum.hpp
+++ Mecanum.hpp
@@ -1,42 +1,49 @@
-#ifndef _MECANUM_HPP_
-#define _MECANUM_HPP_
+#ifndef MECANUM_HPP_
+#define MECANUM_HPP_
 
-#include "mbed.h"
-#include "math.h"
+#include <cmath>
+#include <vector>
 
 class Mecanum {
-
 private:
-
   const float PI = 3.1415;
-
-  float _targetSpeed;
-  float _targetRotation;
-  float _wheel[4];
+  float target_speed_;
+  float target_rotation_;
+  std::vector<float> wheel_speeds_ = {0.0f, 0.0f, 0.0f, 0.0f};
 
 public:
-
-  Mecanum()
-  {};
-
-  void control(float _joyXValue, float _joyYValue, float _turn)
-  {
-    _targetSpeed    = sqrt( (_joyXValue * _joyXValue) + (_joyYValue * _joyYvalue) );
-    _targetRotation = atan2(_joyYValue, _joyXValue) - (PI /4);
-    _targetRotation < 0 ? _targetRotation *= -1 : _targetRotation;
-
-    _wheel[0] = sin(_targetRotation) / cos(_targetRotation) * _targetSpeed - _turn;
-    _wheel[1] = cos(_targetRotation) / sin(_targetRotation) * _targetSpeed + _turn * -1;
-    _wheel[2] = cos(_targetRotation) / sin(_targetRotation) * _targetSpeed - _turn;
-    _wheel[3] = sin(_targetRotation) / cos(_targetRotation) * _targetSpeed + _turn * -1;
+  Mecanum(): target_speed_(0), target_rotation_(0) {
   }
 
+  // 制御ロジックを計算する関数
+  void control(float joy_x, float joy_y, float turn) {
+    target_speed_ = std::sqrt((joy_x * joy_x) + (joy_y * joy_y));
+    target_rotation_ = std::atan2(joy_y, joy_x) - (PI / 4);
+
+    if (target_rotation_ < 0) {
+      target_rotation_ *= -1;
+    }
+
+    wheel_speeds_[0] =
+        std::sin(target_rotation_) / std::cos(target_rotation_) * target_speed_ + turn;
+    wheel_speeds_[1] =
+        std::cos(target_rotation_) / std::sin(target_rotation_) * target_speed_ * -1 + turn;
+    wheel_speeds_[2] =
+        std::cos(target_rotation_) / std::sin(target_rotation_) * target_speed_ + turn;
+    wheel_speeds_[3] =
+        std::sin(target_rotation_) / std::cos(target_rotation_) * target_speed_ * -1 + turn;
+  }
+
+  // 指定したホイール速度を取得する関数
   float getSpeed(int unit) {
-    if(unit < 0 || unit > 3) {
+    if (unit < 0 || unit > 3) {
       return 0.0f;
     }
-    return _wheel[unit];
+    return wheel_speeds_[unit];
   }
+
+  // 全ホイール速度を取得する関数
+  std::vector<float> getAllSpeeds() const { return wheel_speeds_; }
 };
 
-#endif
+#endif  // MECANUM_HPP_
